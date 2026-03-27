a0ea8dcc367eee75a82ad546984a09a0714b5145
MROTA-47 <cabreragonzalezrene@gmail.com>
2026-03-27 09:53:06 -0600

    listo solo workbench y queda

--- backend-api/controllers/productosController.js
+++
@@ -1,7 +0,0 @@
-const db = require('../models');
-const Producto = db.tbc_productos;
-
-exports.getProductos = async (req, res) => {
-  const productos = await Producto.findAll();
-  res.json(productos);
-};
